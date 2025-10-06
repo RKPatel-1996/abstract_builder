c2d566059dee461675275c542a04421ebbdfcb61
RKPatel-1996 <rrohit3rock@gmail.com>
2025-10-06 23:50:36 +0530

    vite modification

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS LINE:
+  base: "/abstract_builder/", // Replace 'your-repo-name' with your repository's name
 });
